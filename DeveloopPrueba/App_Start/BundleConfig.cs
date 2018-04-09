f0886d46afa345012f571d4a53da1508957f3851
germmand <german.aguilera17@gmail.com>
2018-04-09 18:09:21 -0400

    Creando estructura básica del proyecto y agregando MDB.

---
+++ DeveloopPrueba/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace DeveloopPrueba.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // MDB Styles
+            bundles.Add(new StyleBundle("~/Content/MDBStyles")
+                .Include("~/Content/bootstrap*")
+                .Include("~/Content/mdb*"));
+
+            // MDB Scripts.
+            bundles.Add(new ScriptBundle("~/Scripts/MDBScripts")
+                .Include("~/Scripts/bootstrap*")
+                .Include("~/Scripts/jquery-*")
+                .Include("~/Scripts/mdb*")
+                .Include("~/Scripts/popper*"));
+
+            // Custom Styles
+            bundles.Add(new StyleBundle("~/Content/CustomStyle")
+                .Include("~/Content/style.css"));
+        }
+    }
+}
--- DeveloopPrueba/Global.asax.cs
+++ DeveloopPrueba/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DeveloopPrueba.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace DeveloopPrueba
@@ -13,6 +15,10 @@ namespace DeveloopPrueba
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Habilitando las optimizaciones de nombre para los bundles.
+            BundleTable.EnableOptimizations = true;
         }
     }
 }

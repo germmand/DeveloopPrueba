6fd228e3e38eb530c987dadf5c34f39b8552f005
germmand <german.aguilera17@gmail.com>
2018-04-09 18:27:29 -0400

    agregando owin y habilitando cors al middleware

---
+++ DeveloopPrueba/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+
+[assembly: OwinStartup(typeof(DeveloopPrueba.Startup))]
+
+namespace DeveloopPrueba
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            // Habilitando CORS para el Middleware.
+            app.UseCors(CorsOptions.AllowAll);
+        }
+    }
+}

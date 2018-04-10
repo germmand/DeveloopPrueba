91e70623e0fa926eec8bdef42630eed34b64eb02
germmand <german.aguilera17@gmail.com>
2018-04-10 13:43:28 -0400

    Agregando modelo DTO de Encargos.

---
+++ DeveloopPrueba/Models/DTOs/EncargoModelDTO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DeveloopPrueba.Models.DTOs
+{
+    public class EncargoModelDTO
+    {
+        [Required(ErrorMessage = "El campo ID es obligatorio.")]
+        public int EncargoId { get; set; }
+
+        [Required(ErrorMessage = "El campo Albarán es obligatorio.")]
+        [MaxLength(10, ErrorMessage = "La longitud máxima de Albarán es de 10 caracteres.")]
+        public string Albaran { get; set; }
+
+        [Required(ErrorMessage = "El campo Destinatario es obligatorio.")]
+        [MaxLength(28, ErrorMessage = "La longitud máxima de Destinatario es de 28 caracteres.")]
+        public string Destinatario { get; set; }
+
+        [Required(ErrorMessage = "El campo Dirección es obligatorio.")]
+        [MaxLength(250, ErrorMessage = "La longitud máxima de Dirección es de 250 caracteres.")]
+        public string Direccion { get; set; }
+
+        [MaxLength(10, ErrorMessage = "La longitud máxima de Población es de 10 caracteres.")]
+        public string Poblacion { get; set; }
+
+        [Required(ErrorMessage = "El campo CP es obligatorio.")]
+        [MaxLength(5, ErrorMessage = "La longitud requerida para el campo CP es de 5 caracteres.")]
+        [MinLength(5, ErrorMessage = "La longitud requerida para el campo CP es de 5 caracteres.")]
+        public string CP { get; set; }
+
+        [Required(ErrorMessage = "El campo Provincia es obligatorio.")]
+        [MaxLength(20, ErrorMessage = "La longitud máxima de Provincia es de 20 caracteres.")]
+        public string Provincia { get; set; }
+
+        [Required(ErrorMessage = "El campo Teléfono es obligatorio.")]
+        [MaxLength(10, ErrorMessage = "La longitud máxima de Teléfono es de 10 caracteres.")]
+        public string Telefono { get; set; }
+
+        [MaxLength(500, ErrorMessage = "La longitud máxima de Observaciones es de 500 caracteres.")]
+        public string Observaciones { get; set; }
+
+        [Required(ErrorMessage = "El campo Fecha es obligatorio.")]
+        public DateTime Fecha { get; set; }
+    }
+}

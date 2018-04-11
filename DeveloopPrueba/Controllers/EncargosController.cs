2f6f9da700d6b6f965695b9424aeee607c66a1e3
germmand <german.aguilera17@gmail.com>
2018-04-11 14:51:42 -0400

    agregando endpoint para recibir fichero excel.

--- DeveloopPrueba/Controllers/EncargosController.cs
+++ DeveloopPrueba/Controllers/EncargosController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace DeveloopPrueba.Controllers
@@ -15,6 +18,27 @@ namespace DeveloopPrueba.Controllers
 
         }
 
-        // API Methods will go here...
+        [HttpPost]
+        [Route("UploadFile")]
+        public IHttpActionResult UploadFile()
+        {
+            HttpRequest httpRequest = HttpContext.Current.Request;
+            if(httpRequest.Files.Count <= 0)
+            {
+                return BadRequest("No se ha enviado ningún archivo.");
+            }
+
+            // Se obtiene el archivo y se guarda en la carpeta Temporary.
+            HttpPostedFile postedFile = httpRequest.Files[0];
+            string fileExtension = postedFile.FileName.Split('.').Last();
+            string filePath = HttpContext.Current.Server.MapPath("~/Temporary/" + Guid.NewGuid().ToString("n") + "." + fileExtension);
+            postedFile.SaveAs(filePath);
+
+            // Se elimina el archivo del a carpeta Temporary.
+            File.Delete(filePath);
+
+            // Se retorna toda la información al frontend.
+            return Ok();
+        }
     }
 }
---
+++ DeveloopPrueba/Temporary/Readme.txt
@@ -0,0 +1,2 @@
+﻿En esta carpeta se almacenarán los archivos Excels que se suban temporalmente para su procesamiento.
+Una vez que el archivo haya sido procesado, éste será eliminado de forma automática.

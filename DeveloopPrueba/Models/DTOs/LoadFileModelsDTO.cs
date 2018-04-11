413d41db2aa475c4b485cc52b64d8b9fd5d19c44
germmand <german.aguilera17@gmail.com>
2018-04-11 19:30:54 -0400

    agregando modelos para errores de validacion

---
+++ DeveloopPrueba/Models/DTOs/LoadFileModelsDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeveloopPrueba.Models.DTOs
+{
+    public class ErrorEncargoModelDTO
+    {
+        public string PropertyName { get; set; }
+        public string ErrorDescription { get; set; }
+    }
+
+    public class ValidacionEncargoModelDTO
+    {
+        public EncargoModelDTO Encargo { get; set; }
+        public ICollection<ErrorEncargoModelDTO> ValidationErrors { get; set; }
+    }
+}

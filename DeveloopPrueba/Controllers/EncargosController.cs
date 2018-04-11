1342d608945a49c135b3c91ea74953234f9ff5f8
germmand <german.aguilera17@gmail.com>
2018-04-11 15:56:32 -0400

    leyendo data de fichero excel y retornándola en formaton json

--- DeveloopPrueba/Controllers/EncargosController.cs
+++ DeveloopPrueba/Controllers/EncargosController.cs
@@ -1,6 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using DeveloopPrueba.Models.DTOs;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -28,17 +31,68 @@ namespace DeveloopPrueba.Controllers
                 return BadRequest("No se ha enviado ningún archivo.");
             }
 
+            // Lista donde se almacenarán los datos del fichero.
+            ICollection<EncargoModelDTO> encargosXls = new List<EncargoModelDTO>();
+
             // Se obtiene el archivo y se guarda en la carpeta Temporary.
             HttpPostedFile postedFile = httpRequest.Files[0];
             string fileExtension = postedFile.FileName.Split('.').Last();
             string filePath = HttpContext.Current.Server.MapPath("~/Temporary/" + Guid.NewGuid().ToString("n") + "." + fileExtension);
             postedFile.SaveAs(filePath);
 
+            // Se procede a leer la información en el fichero.
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=Excel 8.0";
+            OleDbConnection xlsConnection = new OleDbConnection(connectionString);     
+            try
+            {
+                xlsConnection.Open();
+                // Ésto se hace con la intención de hacer la lectura de páginas general.
+                // Siempre se leerá la primera página independientemente del nombre que ésta tenga asignada.
+                DataTable sheets = xlsConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                string sheetName = sheets.Rows[0]["TABLE_NAME"].ToString();
+
+                // Se crea la query para la lectura completa del archivo.
+                OleDbCommand command = new OleDbCommand(String.Format("SELECT * FROM [{0}]", sheetName), xlsConnection);
+                OleDbDataAdapter adapter = new OleDbDataAdapter()
+                {
+                    SelectCommand = command
+                };
+
+                // Se llena un DataSet con la información del fichero.
+                DataSet data = new DataSet();
+                adapter.Fill(data);
+
+                // Se llena la colleción con la información en el fichero.
+                foreach(DataRow row in data.Tables[0].Rows)
+                {
+                    encargosXls.Add(new EncargoModelDTO()
+                    {
+                        EncargoId       = 0,
+                        Albaran         = row["albaran"]        as string,
+                        Destinatario    = row["destinatario"]   as string,
+                        Direccion       = row["direccion"]      as string,
+                        Poblacion       = row["poblacion"]      as string,
+                        CP              = row["cp"]             as string,
+                        Provincia       = row["provincia"]      as string,
+                        Telefono        = row["telefono"]       as string,
+                        Observaciones   = row["observaciones"]  as string,
+                        Fecha           = DateTime.Now
+                    });
+                }
+            } catch(Exception ex)
+            {
+                File.Delete(filePath);
+                return BadRequest("Error intentando leer el fichero: " + ex.ToString());
+            } finally
+            {
+                xlsConnection.Close();
+            }
+
             // Se elimina el archivo del a carpeta Temporary.
             File.Delete(filePath);
 
             // Se retorna toda la información al frontend.
-            return Ok();
+            return Ok(new { Encargos = encargosXls });
         }
     }
 }
